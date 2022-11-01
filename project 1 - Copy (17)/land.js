fe97be150eb63cf4e2145944ce81b141e1ef41cb
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2022-11-01 09:29:45 +0300

    method chaining

---
+++ project 1 - Copy (17)/land.js
@@ -0,0 +1,33 @@
+// creating a function land()
+function land() {
+  this.area = "";
+  this.status = "for sale";
+}
+
+// setting status open for sale
+land.prototype.open = function () {
+  this.status = "open for sale";
+  return this;
+};
+
+// setting status not for sale
+land.prototype.close = function () {
+  this.status = "not for sale";
+  return this;
+};
+
+// setting parameters
+land.prototype.setParams = function (area) {
+  this.area = area;
+  return this;
+};
+
+// prining land status
+land.prototype.doorstatus = function () {
+  console.log("The", this.area, "land is", this.status);
+  return this;
+};
+
+// creating a land objcet
+var land = new land();
+land.setParams("500 sq ft").close().doorstatus().open().doorstatus();
